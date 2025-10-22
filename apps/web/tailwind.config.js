61b25fc8008e81cb41b7a78b3fc3732c2de8ccc3
William Reed <w.reed87@gmail.com>
2025-10-22 16:27:06 +0200

    tailwhip bug fix 1.0

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,6 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
-  darkMode: "media", // or "class" if you later add a theme toggle
+  darkMode: "media",
   plugins: [],
 }
